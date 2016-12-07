1247605fc9a288e737502b09c4c6eb95d811c99b
Vance Morris <vmorris@us.ibm.com>
2016-12-06 23:49:57 -0600

    add day 7 solutions

---
+++ 07/SSLcount.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+def tokenize_IP(ip):
+  ''' given an IP string, return two lists, one which contains the contents of any found square brakets
+      and second list contains strings outside square brackets. '''
+  inside = []
+  outside = []
+  tokens = ip.split('[')
+  for token in tokens:
+    if ']' in token:
+      # inside, followed by outside
+      a,b = token.split(']')
+      inside.append(a)
+      outside.append(b)
+    else:
+      # outside found
+      outside.append(token)
+  return inside, outside
+
+def collect_ABA(line):
+  ''' given a line, search for any ABA and return a list of sequences'''
+  results = []
+  for i in range(len(line)-2):
+    if line[i] == line[i+2] and line[i] != line[i+1]:
+      results.append(line[i:i+3])
+  return results
+
+
+if __name__ == "__main__":
+
+  count = 0
+
+  with open('input.txt', 'r') as f:
+    for line in f:
+
+      SSLFound = False
+
+      inside, outside = tokenize_IP(line.strip())
+      
+      for o in outside:
+        for aba in collect_ABA(o):
+          #generate bab string
+          bab = aba[1]+aba[0]+aba[1]
+          for i in inside:
+            if bab in i:
+              SSLFound = True
+
+      if SSLFound:
+        count += 1
+
+  print(count)
---
+++ 07/TLScount.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+def tokenize_IP(ip):
+  ''' given an IP string, return two lists, one which contains the contents of any found square brakets
+      and second list contains strings outside square brackets '''
+  inside = []
+  outside = []
+  tokens = ip.split('[')
+  for token in tokens:
+    if ']' in token:
+      # inside, followed by outside
+      a,b = token.split(']')
+      inside.append(a)
+      outside.append(b)
+    else:
+      # outside found
+      outside.append(token)
+  return inside, outside
+
+def has_ABBA(line):
+  ''' given a line, search for ABBA pattern '''
+  for i in range(len(line)-3):
+    if line[i] == line[i+3] and line[i+1] == line[i+2] and line[i] != line[i+1]:
+      return True
+  return False
+
+if __name__ == "__main__":
+  count = 0
+  with open('input.txt', 'r') as f:
+    for line in f:
+      inside, outside = tokenize_IP(line.strip())
+      good_line = False
+      for i in outside:
+        if has_ABBA(i):
+          good_line = True
+      for i in inside:
+        if has_ABBA(i):
+          good_line = False
+      if good_line:
+        count += 1
+
+  print(count)
